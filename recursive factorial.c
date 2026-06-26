7f3189f91640c143428d12f4555b6912a1848740
Khushi chaudhary <Khushi chaudhary@.com>
2026-06-26 18:53:53 +0530

    day 7

---
+++ recursive factorial.c
@@ -0,0 +1,9 @@
+recursive factorial
+#include<stdio.h>
+int factorial(int n) {
+    if (n == 0) {
+        return 1; // Base case: factorial of 0 is 1
+    } else {
+        return n * factorial(n - 1); // Recursive case
+    }
+}
